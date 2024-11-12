b52426d4642436892ec78bae9545c5924f012513
Broedt <164227660+Broedt@users.noreply.github.com>
2024-11-12 17:12:00 +0100

    library is working, still needs a bit more css to make it better looking

--- script.js
+++ script.js
@@ -10,6 +10,39 @@ function Book(title, author, pages, alreadyRead){
     this.alreadyRead = alreadyRead;
 };
 
+Book.prototype.toggleReadStatus = function(){
+    this.alreadyRead = !this.alreadyRead;
+}
+function toggleReadStatus(index){
+    myLibrary[index].toggleReadStatus();
+    renderBooks();
+
+}
+
+function renderBooks(){
+    const displayArea = document.getElementById("displayArea")
+    displayArea.innerHTML="";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookCard = document.createElement("div");
+        bookCard.innerHTML = `
+        <h3 class="title">title: ${book.title}</h3>
+        <h4 class="author">author: ${book.author}</h4>
+        <div class="pages">pages: ${book.pages}</div>
+        <p></p>
+        <div class="read" >${book.alreadyRead ? "Read" : "Not Read Yet" }</div>
+        <button class="toggleReadStatus" onclick="toggleReadStatus(${i})">Change Status</button>
+        <p></p>
+        <button class="removeBookBtn" onclick="removeBookCard(${i})">Delete Book</button>
+       
+        `;
+        bookCard.classList.add('bookCard');
+
+    displayArea.appendChild(bookCard);
+    }
+
+}
+
 function addBookToLibrary(){
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
@@ -18,15 +51,10 @@ function addBookToLibrary(){
     let newBook = new Book(title, author, pages, alreadyRead);
 
     myLibrary.push(newBook);
+    
+    renderBooks();
 
-    function displayNewBook(){
-        const displayArea = document.getElementById("displayArea")
-
-        myLibrary.forEach()
-
-}
-
-    console.log(newBook);
+    console.log(myLibrary);
 };
 
 newBookBtn.addEventListener("click", function(){
@@ -45,4 +73,9 @@ addBook.addEventListener("click", function(){
     checkbox.checked = false;
     })
 });
+
+function removeBookCard(index) {
+    myLibrary.splice(index, 1);
+    renderBooks();
+}
 
