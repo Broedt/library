b6447ec0b0e8abf3369838080bdbffb91693a9b2
Broedt <164227660+Broedt@users.noreply.github.com>
2024-11-13 11:34:26 +0100

    project is done for now, might come back and make the bookCards better looking

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const myLibrary = [];
-let newBookBtn = document.getElementById("newBookBtn");
+
 let addBook = document.getElementById("addBook");
 let allInputs = document.querySelectorAll("#newBookForm input");
+let newBookForm = document.getElementById("newBookForm")
 
 function Book(title, author, pages, alreadyRead){
     this.title = title;
@@ -31,10 +32,11 @@ function renderBooks(){
         <div class="pages">pages: ${book.pages}</div>
         <p></p>
         <div class="read" >${book.alreadyRead ? "Read" : "Not Read Yet" }</div>
+        <div class="cardBtns">
         <button class="toggleReadStatus" onclick="toggleReadStatus(${i})">Change Status</button>
-        <p></p>
-        <button class="removeBookBtn" onclick="removeBookCard(${i})">Delete Book</button>
-       
+        
+        <button class="removeBookBtn" onclick="removeBookCard(${i})"><svg xmlns="http://www.w3.org/2000/svg" width="30px" viewBox="0 0 24 24"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg></button>
+       <d/iv>
         `;
         bookCard.classList.add('bookCard');
 
@@ -59,19 +61,23 @@ function addBookToLibrary(){
 
 newBookBtn.addEventListener("click", function(){
     
-    let newBookForm = document.getElementById("newBookForm")
     newBookForm.style.display = "grid";
-    addBook.style.display ="grid";
+    
 });
 
-addBook.addEventListener("click", function(){    
+newBookForm.addEventListener("submit", function(){ 
+    event.preventDefault()   
     addBookToLibrary();
     allInputs.forEach(input =>{
         input.value = "";           //clears input 
+        
     
     let checkbox = document.getElementById("newBookForm").querySelector("input[type='checkbox']");
     checkbox.checked = false;
     })
+    let submitBtn= document.getElementById("submitBtn");
+    submitBtn.value = "add Book";
+    newBookForm.style.display="none";
 });
 
 function removeBookCard(index) {
